3ced4625842e22e266c0e79c824e5c3143aa9cee
Glenn Madzonga <glenn.madzonga@gmail.com>
2024-11-06 19:53:59 +0200

    added dotenv config in the appropriate files

--- src/kysely-config.ts
+++ src/kysely-config.ts
@@ -1,6 +1,8 @@
 // kysely-config.ts
 import { ColumnType, Generated, Kysely, PostgresDialect } from 'kysely';
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Define the database schema type
 export interface DatabaseSchema {
@@ -35,10 +37,10 @@ const db = new Kysely<DatabaseSchema>({
   dialect: new PostgresDialect({
     pool: new Pool({
       host: process.env.DB_HOST,
-      port:  parseInt(process.env.DB_PORT || "5432"),
-      database:  process.env.DB_NAME,
-      user:  process.env.DB_USER,
-      password:  process.env.DB_PASSWORD, 
+      port: Number(process.env.DB_PORT) || 5432,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD, 
     }),
   }),
 });
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    // Swagger setup
+    const config = new DocumentBuilder()
+    .setTitle('My Application API')
+    .setDescription('API documentation for My Application')
+    .setVersion('1.0')
+    .addBearerAuth()  // Adds JWT Bearer token authentication
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
